dbf533b33753b5a4cae9145532a4dcb5fb8d8d0d
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-14 14:37:16 +0200

    Ejercicios nuevos

---
+++ T2_E4_Arrays/Actividad4/Actividad4/Arrays.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad4
+{
+    public class Arrays
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int minValorArray(int[] array)
+        {
+
+            if (array.Length == 0)
+                return 0;
+
+            int minimo = array[0];
+
+            for (int i = 1; i<array.Length; i++){
+                if (array[i] < minimo)
+                {
+                    minimo = array[i];
+                }
+            }
+
+            return minimo;
+
+            throw new NotImplementedException();
+        }
+        public static int[] invertirArray(int[] array)
+        {
+        
+            // O
+            // Array.Reverse(array);
+
+            int[] arrayInvertido = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayInvertido[i] = array[array.Length-i-1];
+            }
+
+            return arrayInvertido;
+            throw new NotImplementedException();
+        }
+
+        public static int comprobarValorEnArray(int[] array, int valor)
+        {
+
+            //O se podria usar solamente 
+            //return Array.IndexOf(array, valor);
+
+            if (array.Length == 0) return -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(valor == array[i])
+                   return i;
+            }
+
+            return -1;
+            throw new NotImplementedException();
+        }
+
+        public static int calcularModa(int[] entrada)
+        {
+            var diccionarioModa = new Dictionary<int, int>();
+
+            foreach (int i in entrada)
+            {
+                if (diccionarioModa.ContainsKey(i))
+                {
+                    diccionarioModa[i]++;
+                }
+                else
+                {
+                    diccionarioModa.Add(i, 1);
+                }
+            }
+
+            int moda = 0;
+
+            foreach (var item in diccionarioModa)
+            {
+                if(item.Value > moda)
+                {
+                    moda = item.Key;
+                }
+            }
+
+            return moda;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ T2_E5_String/Actividad5/Actividad5.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Actividad5
+{
+    public class Actividad5
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string devolverString(string[] vs)
+        {
+            return String.Join("", vs);
+
+            throw new NotImplementedException();
+        }
+
+        public static string devolverStringOrdenado(string[] vs)
+        {
+
+            Array.Sort(vs);
+            return String.Join("",vs);
+            throw new NotImplementedException();
+        }
+
+        public static int contarCaracter(string[] vs, char v)
+        {
+
+            int contador = 0;
+
+
+            foreach (var item in vs)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+
+                    if(item[i] == v)
+                    {
+                        contador++;
+                    }
+                    
+                }
+            }
+
+
+            return contador;
+
+            throw new NotImplementedException();
+        }
+
+        public static int[] sumarArrays(int[] vs1, int[] vs2)
+        {
+            if(vs1.Length != vs2.Length) 
+                return new int[vs1.Length];
+
+            int[] result = new int[vs2.Length];
+
+            for (int i = 0; i < vs1.Length; i++)
+            {
+                result[i] = vs1[i] + vs2[i];
+            }
+
+            return result;
+
+            throw new NotImplementedException();
+        }
+
+        public static int[] multiplicarArrays(int[] vs1, int[] vs2)
+        {
+            if (vs1.Length != vs2.Length)
+                return new int[vs1.Length];
+
+            int[] result = new int[vs2.Length];
+
+            for (int i = 0; i < vs1.Length; i++)
+            {
+                result[i] = vs1[i] * vs2[i];
+            }
+
+            return result;
+
+            throw new NotImplementedException();
+        }
+
+        public static bool isPalindromo(string entrada)
+        {
+            string entradaLimpia = entrada.ToLower().Replace(" ","");
+
+            int i = 0;
+            int j = entradaLimpia.Length - 1;
+
+            while (i < j)
+            {
+                if (entradaLimpia[i] != entradaLimpia[j])
+                    return false;
+                i++;
+                j--;
+            }
+
+            return true;
+
+            throw new NotImplementedException();
+
+
+        }
+
+        public static int contarPalabras(string entrada)
+        {
+            if(entrada.Length == 0) return 0;
+
+            string[] entradaS = entrada.Split(" ");
+            return entradaS.Length;
+
+            throw new NotImplementedException();
+        }
+
+        public static int contarCaracteres(string entrada)
+        {
+            if (entrada.Length == 0) return 0;
+
+            entrada = entrada.Replace(" ", "");
+
+            return entrada.Length;
+
+            throw new NotImplementedException();
+        }
+    }
+}
