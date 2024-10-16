3aa2ddd75a37b8b8c9826abf2d4904091eaa3e29
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-16 17:06:14 +0200

    Errores

--- Tema2Ejercicios/Ejercicio1.cs
+++ Tema2Ejercicios/Ejercicio1.cs
@@ -1,12 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 var listaNumeros = Enumerable.Range(1, 100);
 
-var listaDivisibles = listaNumeros.Where(x => x%7==0).GroupBy(x => x % 2 == 0 ? "Par" : "Impar");
+var listaDivisiblesDividida = listaNumeros.Where(x => x % 7 == 0).GroupBy(x => x % 2 == 0 ? "Par" : "Impar");
 
-foreach (var grupo in listaDivisibles)
+foreach (var grupo in listaDivisiblesDividida)
 {
+    Console.WriteLine(grupo.Key);
     foreach (var numero in grupo)
     {
-        Console.WriteLine($"{grupo.Key}: {numero}");
+        Console.WriteLine($" - {numero}");
     }
 }
