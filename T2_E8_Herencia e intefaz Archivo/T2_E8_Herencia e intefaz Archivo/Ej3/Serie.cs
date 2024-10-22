1b39e4019c456aed31c426515a78075f78b187e6
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-22 14:40:31 +0200

    Act

---
+++ T2_E8_Herencia e intefaz Archivo/T2_E8_Herencia e intefaz Archivo/Ej3/Serie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_E8_Herencia_e_intefaz_Archivo.Ej3
+{
+    internal class Serie
+    {
+    }
+}
---
+++ T2_E9_Expresiones regulares/Actividad9/Actividad09/Actividad09/Actividad9.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Actividad09
+{
+    public class Actividad9
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool cadenaContenedora(string cadena, string valor)
+        {
+            return Regex.IsMatch(cadena, valor, RegexOptions.None);
+        }
+
+        public static bool numeroEntero(string v)
+        {
+            String patron = @"^\d{1,15}\z";
+
+            return Regex.IsMatch(v, patron);
+        }
+
+        public static bool isSpanish(string telefono)
+        {
+            //\+((00)?34)
+            throw new NotImplementedException();
+        }
+
+        public static bool isCorrectEmail(string email)
+        {
+            String patron = @"^([\w\.\-\+]+)@([\w\-]+)((\.(\w){2,})+)";
+
+            return Regex.IsMatch(email, patron);
+        }
+
+        public static bool numeroPositivo(string v)
+        {
+            String patron = @"^\d{1,15}\z";
+
+            return Regex.IsMatch(v, patron);
+        }
+
+        public static bool isOctal(string v)
+        {
+            String patron = @"^[0-7]+\z";
+
+            return Regex.IsMatch(v, patron);
+        }
+
+        public static bool dni(string v)
+        {
+            String patron = @"^\d{8}[A-Z]\z";
+
+            return Regex.IsMatch(v, patron);
+        }
+
+        public static bool fechaFormat(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool isBinario(string v)
+        {
+            String patron = @"^[1,0]+\z";
+
+            return Regex.IsMatch(v, patron);
+        }
+    }
+}
