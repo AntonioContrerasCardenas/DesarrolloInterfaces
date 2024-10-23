796ef8882743b48ca7f946f40e2bf3ca4b59e6bd
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-23 14:39:32 +0200

    C

--- T2_E8_Herencia e intefaz Archivo/T2_E8_Herencia e intefaz Archivo/Ej3/Serie.cs
+++ T2_E8_Herencia e intefaz Archivo/T2_E8_Herencia e intefaz Archivo/Ej3/Serie.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace T2_E8_Herencia_e_intefaz_Archivo.Ej3
 {
-    internal class Serie
+    public class Serie
     {
+        public const int NUMERO_TEMPORADAS= 3;
+        public const Boolean ENTREGADO = false;
+
+        public String Titulo { get; set; } 
+        public int NumeroTemporadas { get; set; } = NUMERO_TEMPORADAS;
+        public Boolean Entregado { get; set; } = ENTREGADO;
+        public String Creador { get; set; } 
+
+        public String Genero { get; set; }
+
+        public Serie()
+        {
+        }
+
+        public Serie(string titulo, string creador)
+        {
+            Titulo = titulo;
+            Creador = creador;
+        }
+
+        public Serie(string titulo, int numeroTemporadas, string creador, string genero)
+        {
+            Titulo = titulo;
+            NumeroTemporadas = numeroTemporadas;
+            Creador = creador;
+            Genero = genero;
+        }
+
+        public override string ToString()
+        {
+            return "Hola desde Serie";
+        }
     }
 }
