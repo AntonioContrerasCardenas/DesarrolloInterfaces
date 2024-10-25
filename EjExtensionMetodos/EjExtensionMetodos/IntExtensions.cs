1a21f1645807430ae2d50a0e9e6871ea3a046acb
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-25 13:11:20 +0200

    Nuevos ejercicios

---
+++ EjExtensionMetodos/EjExtensionMetodos/IntExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjExtensionMetodos
+{
+    public static class IntExtensions
+    {
+        public static void ImprimirHasta(this int input, int desde)
+        {
+              for (int i = desde; i <= input; i++)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+
+        public static void ExtensionList<T> (this List<T> lista)
+        {
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ EjExtensionMetodos/EjExtensionMetodos/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EjExtensionMetodos;
+
+int prueba = 7;
+prueba.ImprimirHasta(3);
+
+List<string> lista = new List<string>();
+lista.Add("A");
+lista.Add("B");lista.Add("C");lista.Add("D");lista.Add("E");
+lista.ExtensionList();
