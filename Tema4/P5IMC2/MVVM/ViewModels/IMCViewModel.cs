ed2f5c1dfb6f405d15d71347d52ca3c067a9d100
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2025-01-20 12:21:15 +0100

    Ejercicio de maui IMC terminado

--- Tema4/P5IMC2/MVVM/ViewModels/IMCViewModel.cs
+++ Tema4/P5IMC2/MVVM/ViewModels/IMCViewModel.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using P5IMC2.MVVM.Models;
+﻿using P5IMC2.MVVM.Models;
 
 namespace P5IMC2.MVVM.ViewModels
 {
     public class IMCViewModel
     {
-        public IMCModel Imcmodel{ get; set; }
+        public IMCModel IMCModel { get; set; }
 
         public IMCViewModel()
         {
-            Imcmodel = new IMCModel
+            IMCModel = new IMCModel
             {
                 Altura = 180,
                 Peso = 73,
             };
+        }
+
+        public void UpdatePeso(double peso)
+        {
+            IMCModel.Peso = peso;
+        }
+        public void UpdateAltura(double altura)
+        {
+            IMCModel.Altura = altura;
         }
     }
 }
--- Tema4/P5IMC2/MVVM/Views/IMCView.xaml.cs
+++ Tema4/P5IMC2/MVVM/Views/IMCView.xaml.cs
@@ -4,9 +4,37 @@ namespace P5IMC2.MVVM.Views;
 
 public partial class IMCView : ContentPage
 {
-	public IMCView()
-	{
-		InitializeComponent();
-		BindingContext = new IMCViewModel();
-	}
+    private IMCViewModel mcViewModel;
+
+    public IMCView()
+    {
+        InitializeComponent();
+        mcViewModel = new IMCViewModel();
+        BindingContext = mcViewModel;
+    }
+
+    private void PesoPointer_ValueChanged(object sender, Syncfusion.Maui.Gauges.ValueChangedEventArgs e)
+    {
+        var peso = PesoPointer.Value;
+        mcViewModel.IMCModel.Peso = peso;
+
+        UpdateIMC();
+    }
+
+    private void HeightPotiner_ValueChanged(object sender, Syncfusion.Maui.Gauges.ValueChangedEventArgs e)
+    {
+        var altura = HeightPotiner.Value;
+        mcViewModel.IMCModel.Altura = altura;
+
+        UpdateIMC();
+    }
+
+    private void UpdateIMC()
+    {
+        var resultado = mcViewModel.IMCModel.Resultado();
+        var clasificacion = mcViewModel.IMCModel.Clasificacion;
+
+        pointer.Value = resultado;
+        label_clasificacion.Text = clasificacion.ToString();
+    }
 }
